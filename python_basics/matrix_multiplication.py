458f14cafb232ffcc60ea2586e5a4ccef82d19d9
Michael Wood <mhwood@uci.edu>
2025-02-25 10:55:44 -0800

    adding exceptions page

---
+++ python_basics/matrix_multiplication.py
@@ -0,0 +1,32 @@
+
+def dot_product(row,col):
+    dot_sum = 0
+    for r in range(len(row)):
+        dot_sum += row[r]*col[r]
+    return(dot_sum)
+
+def matrix_multiplication_2d(A, B):
+
+    # in matrix multiplication, we define the product
+    # AB as the dot product of
+    # each row of A with each column of B
+
+    # get the rows of A
+    row_A1 = A[0]
+    row_A2 = A[1]
+
+    # get the cols of B
+    col_B1 = []
+    col_B2 = []
+    for i in range(len(B)):
+        col_B1.append(B[i][0])
+        col_B2.append(B[i][1])
+
+    # define the result matrix
+    AB = [[], []]
+    AB[0].append(dot_product(row_A1, col_B1))
+    AB[0].append(dot_product(row_A1, col_B2))
+    AB[1].append(dot_product(row_A2, col_B1))
+    AB[1].append(dot_product(row_A2, col_B2))
+
+    return(AB)
