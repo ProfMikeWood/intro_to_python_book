89c35a37ab01cf6881eb964dc47250aa718435e8
Michael Wood <mhwood@uci.edu>
2025-03-05 10:00:18 -0800

    adding packages page

---
+++ code_organization/python_notes/conda/conda_notes.py
@@ -0,0 +1,5 @@
+
+
+
+def conda_environment_setup():
+    print('The command to make a new conda environment is conda create --name my_env')
---
+++ code_organization/python_notes/general_notes.py
@@ -0,0 +1,11 @@
+
+
+def module_import_note():
+    print('A module can be imported in a Jupyter notebook\n'\
+          'if it is in the same directory as the notebook')
+
+
+def package_installation():
+    print('To install a package using pip and a setup file, '\
+          'use the pip install . command')
+
